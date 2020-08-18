0e38c1ab935b65510bb1a817af8c44d0a4c506ea
JoshuaHolloway <jhollow6@asu.edu>
2020-08-18 18:08:31 -0500

    task 6

--- index.js
+++ index.js
@@ -30,51 +30,9 @@ import { getWinnersByYear } from './task-5.js';
 const winners_by_year = getWinnersByYear(getWinners, getYears, getFinals,fifaData);
 console.log(winners_by_year);
 
-/* Task 6: Write a function called `getAverageGoals` that accepts a parameter `data` and returns the the average number of home team goals and away team goals scored per match (Hint: use .reduce and do this in 2 steps) */
-
-function getAverageGoals(/* code here */) {
-
-    /* code here */
-
-};
-
-getAverageGoals();
-
-/// STRETCH 🥅 //
-
-/* Stretch 1: Create a function called `getCountryWins` that takes the parameters `data` and `team initials` and returns the number of world cup wins that country has had. 
-
-Hint: Investigate your data to find "team initials"!
-Hint: use `.reduce` */
-
-function getCountryWins(/* code here */) {
-
-    /* code here */
-
-};
-
-getCountryWins();
-
-
-/* Stretch 3: Write a function called getGoals() that accepts a parameter `data` and returns the team with the most goals score per appearance (average goals for) in the World Cup finals */
-
-function getGoals(/* code here */) {
-
-    /* code here */
-
-};
-
-getGoals();
-
-
-/* Stretch 4: Write a function called badDefense() that accepts a parameter `data` and calculates the team with the most goals scored against them per appearance (average goals against) in the World Cup finals */
-
-function badDefense(/* code here */) {
-
-    /* code here */
-
-};
-
-badDefense();
-
-/* If you still have time, use the space below to work on any stretch goals of your chosing as listed in the README file. */
+/* Task 6 */
+console.log('Task-6');
+import { getAverageGoals } from './task-6.js';
+const {ave_home_goals, ave_away_goals} = getAverageGoals(fifaData);
+console.log(`ave-home-goals-per-game: ${ave_home_goals}`);
+console.log(`ave-away-goals-per-game: ${ave_away_goals}`);
--- task-6.js
+++ task-6.js
@@ -1,1 +1,23 @@
-import { fifaData } from './fifa.js';
+/* Task 6: Write a function called `getAverageGoals` that accepts a parameter `data` and returns the the average number of home team goals and away team goals scored per match (Hint: use .reduce and do this in 2 steps) */
+
+function getAverageGoals(data) {
+
+
+    let sum_home_goals = 0;
+    let sum_away_goals = 0;
+
+    data.forEach((elem) => {
+        sum_home_goals += elem['Home Team Goals'];
+        sum_away_goals += elem['Away Team Goals'];
+    });
+    const N = data.length;
+    const ave_home_goals = sum_home_goals / N;
+    const ave_away_goals = sum_away_goals / N;
+
+    return {ave_home_goals, ave_away_goals};
+};
+
+// repeat with .reduce()
+const getAverageGoals2 = () => {};
+
+export { getAverageGoals, getAverageGoals2 };
