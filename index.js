bc85e68491bb75a5e7dcb0c24fe96802e8c71709
JoshuaHolloway <jhollow6@asu.edu>
2020-08-19 14:00:12 -0500

    stretch 3

--- index.js
+++ index.js
@@ -11,14 +11,14 @@ import { getFinals, getFinals2 } from './task-2.js';
 // console.log(getFinals2(fifaData));
 
 /* Task 1 */
-console.log('Task-1');
-import { get_finals_2014, tasks_1 } from './task-1.js';
-get_finals_2014(fifaData);
-console.log(`Home Team name for 2014 world cup final: ${tasks_1[0]()}`); // a
-console.log(`Away Team name for 2014 world cup final: ${tasks_1[1]()}`); // b
-console.log(`Home Team goals for 2014 world cup final: ${tasks_1[2]()}`); // c
-console.log(`Away Team goals for 2014 world cup final: ${tasks_1[3]()}`); // d
-console.log(`Winner of 2014 world cup final: ${tasks_1[4]()}`); // e
+// console.log('Task-1');
+// import { get_finals_2014, tasks_1 } from './task-1.js';
+// get_finals_2014(fifaData);
+// console.log(`Home Team name for 2014 world cup final: ${tasks_1[0]()}`); // a
+// console.log(`Away Team name for 2014 world cup final: ${tasks_1[1]()}`); // b
+// console.log(`Home Team goals for 2014 world cup final: ${tasks_1[2]()}`); // c
+// console.log(`Away Team goals for 2014 world cup final: ${tasks_1[3]()}`); // d
+// console.log(`Winner of 2014 world cup final: ${tasks_1[4]()}`); // e
 
 
 /* Task 3 */
@@ -42,16 +42,19 @@ console.log(`Winner of 2014 world cup final: ${tasks_1[4]()}`); // e
 // console.log(winners_by_year);
 
 /* Task 6 */
-console.log('Task-6');
-import { getAverageGoals, getAverageGoals2 } from './task-6.js';
-const {ave_home_goals, ave_away_goals} = getAverageGoals(fifaData);
-console.log(`ave-home-goals-per-game: ${ave_home_goals}`);
-console.log(`ave-away-goals-per-game: ${ave_away_goals}`);
+// console.log('Task-6');
+// import { getAverageGoals, getAverageGoals2 } from './task-6.js';
+// const {ave_home_goals, ave_away_goals} = getAverageGoals(fifaData);
+// console.log(`ave-home-goals-per-game: ${ave_home_goals}`);
+// console.log(`ave-away-goals-per-game: ${ave_away_goals}`);
 
-console.log('Task-6 with .reduce():');
-const [ave_home_goals2, ave_away_goals2] = getAverageGoals2(fifaData);
-console.log(`ave-home-goals-per-game: ${ave_home_goals2}`);
-console.log(`ave-away-goals-per-game: ${ave_away_goals2}`);
+// console.log('Task-6 with .reduce():');
+// const [ave_home_goals2, ave_away_goals2] = getAverageGoals2(fifaData);
+// console.log(`ave-home-goals-per-game: ${ave_home_goals2}`);
+// console.log(`ave-away-goals-per-game: ${ave_away_goals2}`);
 
 // Stretch 1:
-import {getCountryWins as stretch_1} from './task-X--stretch.js';
+console.log('\n');
+console.log('Stretch:');
+import {getCountryWins as stretch_1, getGoals} from './task-X--stretch.js';
+getGoals(fifaData);
